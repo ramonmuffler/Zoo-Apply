587400b7853f17ffd9d362c47e30696578574926
michael <michael.hersberger@icloud.com>
2026-03-17 13:27:42 +0100

    Contact side added

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -3,7 +3,7 @@ import "./Contact.css";
 const Contact = () => {
   return (
     <div className="contact">
-      <h2>Kontaktieren Sie uns</h2>
+      <h2>Kontaktieren Sie uns!</h2>
       <p>Adresse: Zürichbergstrasse 221, 8044 Zürich</p>
       <p>Telefon: +49 44 254 25 00</p>
       <p>Email: <a href="mailto:zoo@zoo.ch?subject=Support Anfrage&body=Hallo lieber Zoo Zürich,%0A%0AIch habe folgendes Problem:%0A...%0A%0AVielen Dank.">zoo@zoo.ch</a></p>

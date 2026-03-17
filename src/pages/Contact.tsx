81322f5d87934674fa3e6ed9ed2c1426e8a42744
michael <michael.hersberger@icloud.com>
2026-03-17 13:22:50 +0100

    contact seite

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,15 +1,15 @@
-import './Contact.css';
+import "./Contact.css";
 
 const Contact = () => {
   return (
     <div className="contact">
       <h2>Kontaktieren Sie uns</h2>
-      <p>Adresse: Zoo Straße 1, 12345 Zoo Stadt</p>
-      <p>Telefon: +49 123 456789</p>
-      <p>Email: info@zoo.de</p>
+      <p>Adresse: Zürichbergstrasse 221, 8044 Zürich</p>
+      <p>Telefon: +49 44 254 25 00</p>
+      <p>Email: <a href="mailto:zoo@zoo.ch?subject=Support Anfrage&body=Hallo lieber Zoo Zürich,%0A%0AIch habe folgendes Problem:%0A...%0A%0AVielen Dank.">zoo@zoo.ch</a></p>
       <h3>Öffnungszeiten</h3>
       <p>Montag - Freitag: 9:00 - 18:00</p>
-      <p>Samstag - Sonntag: 10:00 - 19:00</p>
+      <p>Samstag - Sonntag: 9:00 - 18:00</p>
     </div>
   );
 };
